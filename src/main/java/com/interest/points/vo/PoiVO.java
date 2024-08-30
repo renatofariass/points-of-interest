c4c3f24a97d418764ffb5f2604341c8e02fd2753
Renato Farias <santosrenato351@gmail.com>
2024-08-30 14:01:34 -0300

    create PoiVO

--- src/main/java/com/interest/points/vo/PoiVO.java
+++ src/main/java/com/interest/points/vo/PoiVO.java
@@ -1,11 +1,22 @@
 package com.interest.points.vo;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PoiVO {
+    @NotNull
     private String name;
+    @NotNull
     private int x;
+    @NotNull
     private int y;
-    private String category;
+    @NotEmpty
+    private List<String> categories = new ArrayList<>();;
 
     public String getName() {
         return name;
@@ -31,12 +42,12 @@ public class PoiVO {
         this.y = y;
     }
 
-    public String getCategory() {
-        return category;
+    public List<String> getCategories() {
+        return categories;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
     }
 }
 
