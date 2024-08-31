ac7d8efe6647247e7bf194ccab487ab387caf08f
Renato Farias <santosrenato351@gmail.com>
2024-08-30 23:38:41 -0300

    add findPoisByProximity and findPoisByProximityWithoutTimeFilter

--- src/main/java/com/interest/points/service/PoiService.java
+++ src/main/java/com/interest/points/service/PoiService.java
@@ -6,10 +6,13 @@ import com.interest.points.model.Category;
 import com.interest.points.model.Poi;
 import com.interest.points.repository.CategoryRepository;
 import com.interest.points.repository.PoiRepository;
-import com.interest.points.vo.PoiVO;
+import com.interest.points.utils.PoiUtils;
+import com.interest.points.vo.PoiVoRequest;
+import com.interest.points.vo.PoiVoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,38 +25,41 @@ public class PoiService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Poi> getAllPois() {
-        return poiRepository.findAll();
+    public List<PoiVoResponse> findAllPois() {
+        return ModelMapperConverter.parseListObjects(poiRepository.findAll(), PoiVoResponse.class);
     }
 
-    public List<Poi> findPoisByProximity(int refX, int refY, int maxDistance) {
-        return poiRepository.findAll().stream()
-                .filter(poi -> distance(refX, refY, poi.getX(), poi.getY()) <= maxDistance)
+    public List<PoiVoResponse> findPoisByProximity(List<String> categories, int refX, int refY,
+                                                              int maxDistance) {
+        var poisList = poiRepository.findAll().stream()
+                .filter(poi -> categories.isEmpty() ||
+                        poi.getCategories().stream().anyMatch(category -> categories.contains(category.getName())))
+                .filter(poi -> PoiUtils.isWithinProximity(refX, refY, poi))
+                .filter(poi -> PoiUtils.isOpenAt(LocalTime.now(), poi.getOpeningHours(), poi.getClosingHours()))
                 .collect(Collectors.toList());
+
+        return ModelMapperConverter.parseListObjects(poisList, PoiVoResponse.class);
     }
 
-    public List<Poi> findPoisByCategoryAndProximity(List<String> categories, int refX, int refY, int maxDistance) {
-        return poiRepository.findAll().stream()
-                .filter(poi -> poi.getCategories().stream()
-                        .anyMatch(category -> categories.contains(category.getName())))
-                .filter(poi -> distance(refX, refY, poi.getX(), poi.getY()) <= maxDistance)
+    public List<PoiVoResponse> findPoisByProximityWithoutTimeFilter(List<String> categories, int refX, int refY, int maxDistance) {
+        var poisList = poiRepository.findAll().stream()
+                .filter(poi -> PoiUtils.isWithinProximity(refX, refY, poi))
+                .filter(poi -> categories.isEmpty() ||
+                        poi.getCategories().stream().anyMatch(category -> categories.contains(category.getName())))
                 .collect(Collectors.toList());
+
+        return ModelMapperConverter.parseListObjects(poisList, PoiVoResponse.class);
     }
 
-    public Poi createPoi(PoiVO poiVO) {
-
-        Set<Category> categories = poiVO.getCategories().stream()
+    public PoiVoResponse createPoi(PoiVoRequest poiVoRequest) {
+        Set<Category> categories = poiVoRequest.getCategories().stream()
                 .map(categoryName -> categoryRepository.findByName(categoryName)
                         .orElseThrow(() -> new ResourceNotFoundException("Category not found: " + categoryName)))
                 .collect(Collectors.toSet());
 
-        Poi poi = ModelMapperConverter.parseObject(poiVO, Poi.class);
+        Poi poi = ModelMapperConverter.parseObject(poiVoRequest, Poi.class);
         poi.setCategories(categories);
 
-        return poiRepository.save(poi);
-    }
-
-    private double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return ModelMapperConverter.parseObject(poiRepository.save(poi), PoiVoResponse.class);
     }
 }
