1b4f564e590a8ded4470ef89d647d71758def38f
Renato Farias <santosrenato351@gmail.com>
2024-09-02 20:47:32 -0300

    remove table point_categories, add foreign key category_id to points_of_interest table

--- src/main/resources/db/migration/V3__Create_Table_points_of_interest.sql
+++ src/main/resources/db/migration/V3__Create_Table_points_of_interest.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS points_of_interest
     x    INT          NOT NULL,
     y    INT          NOT NULL,
     opening_hours VARCHAR(255) NOT NULL,
-    closing_hours VARCHAR(255) NOT NULL
+    closing_hours VARCHAR(255) NOT NULL,
+    category_id BIGINT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories (id)
 );
--- src/main/resources/db/migration/V4__Create_Table_point_categories.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE point_categories
-(
-    point_id      BIGINT NOT NULL,
-    category_id BIGINT NOT NULL,
-    PRIMARY KEY (point_id, category_id),
-    FOREIGN KEY (point_id) REFERENCES points_of_interest (id),
-    FOREIGN KEY (category_id) REFERENCES categories (id)
-);
